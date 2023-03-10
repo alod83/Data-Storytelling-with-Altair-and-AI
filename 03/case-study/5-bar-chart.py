e2484870eadbb0f5923080f70e90a4cb907e4f64
alod83 <angelica.loduca@iit.cnr.it>
2023-03-10 05:33:39 +0100

    added case study and altair

---
+++ 03/case-study/5-bar-chart.py
@@ -0,0 +1,45 @@
+# Import the required libraries
+# Load '../data/competitions.csv' as pandas dataframe
+# Select only the columns Training Type, Record Time (Seconds), Our Best Time
+# Calculate the percentage difference between columns Our Best Time and Record Time (Seconds)  and store it into a new column called Percentage Difference
+# Draw a bar chart with the following encodings:
+# * The Percentage Difference on the y-axis with the following properties:
+#   - The domain of the Y scale to [0,100]
+# * Training Type on the x-axis with the following properties:
+#   - The values sorted in descending order (-y)
+# Set the following properties of the bar chart:
+# * width to 300 pixels
+# Save the chart as 'competitions.html'
+
+import pandas as pd
+import altair as alt
+
+df = pd.read_csv('../data/competitions.csv')
+df = df[['Training Type', 'Record Time (Seconds)', 'Our Best Time']]
+# Manually change the column name
+df['Percentage Improvement'] = 100 - (df['Our Best Time'] - df['Record Time (Seconds)']) / df['Record Time (Seconds)'] * 100
+
+chart = alt.Chart(df).mark_bar().encode(
+    y=alt.Y('Percentage Improvement', scale=alt.Scale(domain=[0, 100])),
+    x=alt.X('Training Type', sort='-y'),
+    # Add the color encoding. Set the color to:
+    # - #003049 if the Percentage Improvement is greater than 50, 
+    # - lightgray otherwise
+    color=alt.condition(
+        alt.datum['Percentage Improvement'] > 50,
+        alt.value('#003049'),
+        alt.value('lightgray')
+    )
+).properties(
+    width=300,
+    # Add the following properties to the chart:
+    # * title to 'Invest in Rowling and Cycling'
+    title='Invest in Rowling and Cycling'
+)
+
+# Add a horizontal red line to the chart at y=50
+# Add the line to the chart
+
+chart = chart + alt.Chart(pd.DataFrame({'y': [50]})).mark_rule(color='red').encode(y='y')
+
+chart.save('competitions.html')
