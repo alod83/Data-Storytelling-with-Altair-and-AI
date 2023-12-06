b06e0f2ccba5833b14effa68c924ae3b54eb4040
alod83 <angelica.loduca@iit.cnr.it>
2023-12-06 05:17:48 +0100

    added case study comptitions

---
+++ CaseStudies/competitions/1-bar-chart.py
@@ -0,0 +1,24 @@
+# Import the required libraries
+# Load '../data/competitions.csv' as pandas dataframe
+# Select only the columns Training Type, Record Time (Seconds), Our Best Time
+# Use melt to transform the dataframe into a long format, with the parameter id_vars set to 'Training Type'
+# Draw a bar chart named chart in Altair with:
+# * The Training Type column as the x-axis
+# * The value column as the y-axis
+# * The variable column as the color
+# Save the chart as 'competitions.html'
+
+import pandas as pd
+import altair as alt
+
+df = pd.read_csv('source/competitions.csv')
+df = df[['Training Type', 'Record Time (Seconds)', 'Our Best Time']]
+df = df.melt(id_vars='Training Type')
+
+chart = alt.Chart(df).mark_bar().encode(
+    x='Training Type',
+    y='value',
+    color='variable'
+)
+
+chart.save('competitions.html')
