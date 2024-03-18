0aaef9c3198caa0e25e07aa38777a17cb77bdee5
Angelica Lo Duca <angelica.loduca@iit.cnr.it>
2024-03-18 13:00:39 +0100

    Update rag.py

--- 07/rag/rag.py
+++ 07/rag/rag.py
@@ -5,13 +5,13 @@
 # pip install pydantic==1.10.11
 # pip install chromadb
 # pip install tiktoken
-from langchain.document_loaders import UnstructuredHTMLLoader
+from langchain_community.document_loaders.html import UnstructuredHTMLLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 
 # load data
 loader = UnstructuredHTMLLoader('product.html')
@@ -29,10 +29,10 @@ splitted_data = text_splitter.split_documents(data)
 embeddings = OpenAIEmbeddings()
 
 store = Chroma.from_documents(
-    splitted_data, 
-    embeddings, 
+    splitted_data,
+    embeddings,
     ids = [f"{item.metadata['source']}-{index}" for index, item in enumerate(splitted_data)],
-    collection_name='Product-Info', 
+    collection_name='Product-Info',
 persist_directory='db',
 )
 store.persist()
@@ -59,5 +59,5 @@ qa = RetrievalQA.from_chain_type(
 )
 
 print(
-    qa('Describe the product New SmartX 2023 using 30 words')
+    qa.invoke({"query": 'Describe the product New SmartX 2023 using 30 words'})
 )
